c0a7e8631a0f7dc6d0c23366ee7405ecd851ad35
George <geor2335@stud.kea.dk>
2022-03-25 21:33:17 +0100

    final fix

--- src/components/Attribution.jsx
+++ src/components/Attribution.jsx
@@ -1,11 +1,13 @@
 function Attribution() {
   return (
     <footer className="attribution">
-      Challenge by{" "}
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
-        Frontend Mentor
-      </a>
-      . Coded by <a href="#">George Nicolae</a>.
+      <p>
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">George Nicolae</a>.
+      </p>
     </footer>
   );
 }
